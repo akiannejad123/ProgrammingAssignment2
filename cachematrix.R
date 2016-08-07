a8b36716f991fe053cae17cf268d99f202dd3dad
akiannejad123 <amandakiannejad123@hotmail.com>
2016-08-07 14:42:31 -0700

    First successful commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  THis version is by Amanda Kiannejad composed on Aug 6 2016 for the Cousera
+## course R Progamming 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix contructs the diminsions of the matrix 
+## it sets the values and initialize the function for 
+## the solve function which will calculate the inverse
 
+
+makeCacheMatrix <- function(q= matrix()) {
+  matInv <- NULL
+  setCacheMatrix <- function(g) {  #assigns arg to q in the parent environment
+    q <<- g
+    matInv <<- NULL   # NULL value is an object with unseficied dims
+  }
+  getCacheMatrix <- function() q
+  setMatrixInverse <- function(solve) matInv <<- solve 
+  getMatrixInverse <- function() matInv
+  list(setCacheMatrix = setCacheMatrix, getCacheMatrix = getCacheMatrix,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheMatrix <- function(q, ...) {
+  matInv <- q$getMatrixInverse()  
+  if(!is.null(matInv)) {   
+    message("getting cached matrix data")
+    return(m)   #if the matrix is empty nothing is calculated
+  }
+  Mat <- q$getCacheMatrix()  #this retreives the matrix
+  matInv <- solve(Mat, ...)
+  q$setMatrixInverse(matInv)      #this sets the matrix 
+  matInv
 }
